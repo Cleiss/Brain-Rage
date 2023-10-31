fb0a4e2129c42a50d9c94da90bd9ab4e1b4ffe92
Cleiss <cleitonsns@gmail.com>
2023-10-31 17:21:21 -0300

    user model created

---
+++ src/models/users.js
@@ -0,0 +1,49 @@
+import mongoose from 'mongoose'
+import bcrypt from 'bcrypt'
+
+const UserSchema = new mongoose.Schema(
+    {
+        nome: {
+            type: String,
+            required: true
+        },
+        sobrenome: {
+            type: String,
+            required: true
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+            lowercase: true
+        },
+        username: {
+            type: String,
+            required: true,
+            unique: true
+        },
+        senha: {
+            type: String,
+            required: true,
+            select: true
+        },
+        avatar: {
+            type: String,
+            required: true
+        }
+    } /*required é a verificação dos dados no bd*/
+)
+
+UserSchema.pre("save", async function (next) {
+    this.senha = await bcrypt.hash(this.senha, 10)
+
+    /*função pre("save", função a ser executada) = antes de salvar algo, execute a função.
+    neste caso, antes de salvar a senha, criptografe-a*/
+    
+    next()
+})
+/* hash() criptografa a senha; 10 = quant. de salt/rodadas*/
+
+const Users = mongoose.model("Users", UserSchema)
+
+export default Users
