2b916c3ca91e9d7b56516854d80a826f72e16d5a
cleiss <cleitonsns@gmail.com>
2025-04-06 20:15:40 -0300

    cors

--- index.js
+++ index.js
@@ -4,12 +4,14 @@ import dotenv from "dotenv"
 import userRouter from "./src/routes/users.route.js"
 import authRouter from "./src/routes/auth.route.js"
 import swaggerRouter from "./src/routes/swagger.route.cjs"
+import cors from "cors"
 
 const app = express()
 const port = process.env.PORT || 5000
 
 
 dotenv.config() /*dotenv está aqui pq a função que executa o banco de dados está sendo executada aqui no index.js*/
+app.use(cors()) /* módulo que habilita CORS para vários endpoints. evita o problema de Access-Control-Allow-Origin. */
 connectDB()
 
 app.use(express.json())
