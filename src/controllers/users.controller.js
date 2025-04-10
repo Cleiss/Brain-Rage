f7bd237f9fb7936ce3d52ea929768206dd4b790c
Cleiss <cleitonsns@gmail.com>
2025-04-10 11:45:42 -0300

    teste de erro

--- src/controllers/users.controller.js
+++ src/controllers/users.controller.js
@@ -31,7 +31,7 @@ const createUser = async (req, res) => {
 
     }
     catch (err) {
-        return res.status(500).send(err)
+        return res.status(500).send("erro no controller")
     }
 }
 
