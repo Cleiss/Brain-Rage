d343fe32e1c282570673c5aa39cb071dd4266c44
cleiss <cleitonsns@gmail.com>
2025-08-12 20:24:02 -0300

    ajuste de pontuação/seq

--- src/controllers/seq.controller.js
+++ src/controllers/seq.controller.js
@@ -6,12 +6,25 @@ const resetSeq = async (req, res) => {
 
         const id = req.userId
 
+        const user = await usersService.confirmById(id)
+
+        if (!id || !user) {
+            return res.status(401).send('Solicitação não permitida')
+        }
+
         const seqServ = []
         await usersService.updateseqServ(id, seqServ)
+
+        const SeqPlay = []
+        await usersService.updateseqPlay(id, SeqPlay)
+
+        const prov = 0
+        await usersService.updatepontProv(id, prov)
     }
     catch (erro) {
         console.log(erro)
     }
 }
 
-export default {resetSeq}
+
+export default { resetSeq }
